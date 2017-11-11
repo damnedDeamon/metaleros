029241d6f94b6c0bba92f248b08bf5fb56677204
damnedDemon <matiasdiaz199@gmail.com>
2017-11-11 01:26:53 -0300

    Consultar la cantidad de figuras.

--- Poo_Matriz/src/poo_Algebra/Poo_Algebra.java
+++ Poo_Matriz/src/poo_Algebra/Poo_Algebra.java
@@ -74,100 +74,113 @@ public class Poo_Algebra {
         System.out.println("====================================");
 
         Scanner leer = new Scanner(System.in);
-
+        System.out.print("Cuantas figuras desea ingresar: ");
+        int opcion = Integer.parseInt(leer.nextLine());
+        int cont = 0;
         System.out.println("Reglas ");
         System.out.println("Solamente puede ingresar 3 o 4 coordenadas");
-        System.out.println();
-        System.out.print("¿Cuantos puntos desea ingresar?");
+        while (cont < opcion) {
+            cont++;
+            System.out.print("Cuantos puntos desea ingresar: ");
 
-        int op = Integer.parseInt(leer.nextLine());
+            int op = Integer.parseInt(leer.nextLine());
+            System.out.println();
 
-        if (op == 3) {
-            for (int i = 1; i <= op; i++) {
-                System.out.print("Ingrese fila y columna " + i + ": ");
-                String coordenada_1 = leer.nextLine();
-                String[] posicion_1 = coordenada_1.split(",");
+            if (op == 3) {
+                for (int i = 1; i <= op; i++) {
+                    System.out.print("Ingrese fila y columna " + i + ": ");
+                    String coordenada_1 = leer.nextLine();
+                    String[] posicion_1 = coordenada_1.split(",");
 
-                String fila_1 = posicion_1[0];
-                String columna_1 = posicion_1[1];
+                    String fila_1 = posicion_1[0];
+                    String columna_1 = posicion_1[1];
 
-                int fila = Integer.parseInt(fila_1);
-                int columna = Integer.parseInt(columna_1);
-                for (int f = 0; f < matriz.length; f++) {
-                    for (int c = 0; c < matriz[f].length; c++) {
-                        if (f == fila && c == columna) {
-                            matriz[f][c] = " x";
-                        }
+                    int fila = Integer.parseInt(fila_1);
+                    int columna = Integer.parseInt(columna_1);
+
+                    matriz[fila][columna] = " X";
+
+                }
+                System.out.println();
+                System.out.print("f/c ");
+                for (int f = 0; f < matriz[0].length; f++) {
+
+                    // if dejar de poner 0
+                    if (f >= 10) {
+                        System.out.print(f + "  ");
+                    } else {
+                        System.out.print(0);
+                        System.out.print(f + "  ");
                     }
                 }
+                System.out.println(); // enter
 
-            }
-            System.out.print("f/c ");
-            for (int f = 0; f < matriz[0].length; f++) {
-
-                // if dejar de poner 0
-                if (f >= 10) {
-                    System.out.print(f + "  ");
-                } else {
-                    System.out.print(0);
-                    System.out.print(f + "  ");
-                }
-            }
-            System.out.println(); // enter
-
-            // ciclo para las filas de numeros
-            for (int f = 0; f < matriz.length; f++) {
-
-                // if dejar de poner 0 y mostrar la columna de numeros
-                if (f >= 10) {
-                    System.out.print(f);
-                } else {
-                    System.out.print(0);
-                    System.out.print(f);
-                }
                 // ciclo para las filas de numeros
-                for (int c = 0; c < matriz[f].length; c++) {
-                    System.out.print(" " + matriz[f][c] + " ");
+                for (int f = 0; f < matriz.length; f++) {
+
+                    // if dejar de poner 0 y mostrar la columna de numeros
+                    if (f >= 10) {
+                        System.out.print(f);
+                    } else {
+                        System.out.print(0);
+                        System.out.print(f);
+                    }
+                    // ciclo para las filas de numeros
+                    for (int c = 0; c < matriz[f].length; c++) {
+                        System.out.print(" " + matriz[f][c] + " ");
+                    }
+                    System.out.println(); // enter
+                }
+
+            } else if (op == 4) {
+                for (int i = 1; i <= op; i++) {
+                    System.out.print("Ingrese fila y columna " + i + ": ");
+                    String coordenada_2 = leer.nextLine();
+                    String[] posicion_2 = coordenada_2.split(",");
+
+                    String fila_2 = posicion_2[0];
+                    String columna_2 = posicion_2[1];
+
+                    int fil = Integer.parseInt(fila_2);
+                    int colum = Integer.parseInt(columna_2);
+
+                    matriz[fil][colum] = " X";
+
+                }
+                System.out.println();
+                System.out.print("f/c ");
+                for (int f = 0; f < matriz[0].length; f++) {
+
+                    // if dejar de poner 0
+                    if (f >= 10) {
+                        System.out.print(f + "  ");
+                    } else {
+                        System.out.print(0);
+                        System.out.print(f + "  ");
+                    }
                 }
                 System.out.println(); // enter
-            }
 
-        } else if (op == 4) {
-            for (int i = 1; i <= op; i++) {
-                System.out.print("Ingrese fila y columna " + i + ": ");
-                String coordenada_2 = leer.nextLine();
-                String[] posicion_2 = coordenada_2.split(",");
+                // ciclo para las filas de numeros
+                for (int f = 0; f < matriz.length; f++) {
 
-                String fila_2 = posicion_2[0];
-                String columna_2 = posicion_2[1];
-
-                int fil = Integer.parseInt(fila_2);
-                int colum = Integer.parseInt(columna_2);
-
-                System.out.print(matriz[fil][colum] = " X");
+                    // if dejar de poner 0 y mostrar la columna de numeros
+                    if (f >= 10) {
+                        System.out.print(f);
+                    } else {
+                        System.out.print(0);
+                        System.out.print(f);
+                    }
+                    // ciclo para las filas de numeros
+                    for (int c = 0; c < matriz[f].length; c++) {
+                        System.out.print(" " + matriz[f][c] + " ");
+                    }
+                    System.out.println(); // enter
+                }
 
             }
 
         }
-        System.out.println("f/c ");
-        // ciclo para las filas de numeros
-        for (int f = 0; f < matriz.length; f++) {
-
-            // if dejar de poner 0 y mostrar la columna de numeros
-            if (f >= 10) {
-                System.out.print(f);
-            } else {
-                System.out.print(0);
-                System.out.print(f);
-            }
-
-            // ciclo para las filas de numeros
-            for (int c = 0; c < matriz[f].length; c++) {
-                System.out.print(" " + matriz[f][c] + " ");
-            }
-            System.out.println(); // enter
-        }
-        
 
     }
 
