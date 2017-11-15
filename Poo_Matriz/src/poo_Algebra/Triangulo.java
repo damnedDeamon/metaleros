5a9e98327d82a84a52e8c1c7e088b1434787a7fd
daisydaniela36 <daisydaniela36@gmail.com>
2017-11-15 20:30:59 -0300

    cree un calculo para poder validar que no sea una linea y que sea una figura

--- Poo_Matriz/src/poo_Algebra/Triangulo.java
+++ Poo_Matriz/src/poo_Algebra/Triangulo.java
@@ -9,13 +9,15 @@ public class Triangulo {
     public Triangulo(int fil_1, int fil_2, int colum_1, int colum_2, int fil_3, int colum_3) {
         this.fil_1 = fil_1;
         this.fil_2 = fil_2;
+        this.fil_3 = fil_3;
         this.colum_1 = colum_1;
         this.colum_2 = colum_2;
-        this.fil_3 = fil_3;
         this.colum_3 = colum_3;
     }
-
-    public void equilatero() {
+    public void verificacion(){
+        
+    }
+    public void calculoTriangulo() {
         // Calculo de las figuras
         double resta_1, resta_2, suma_1;
         double multi_1, multi_2, raiz_cuadrada = 0, potencia_1, potencia_2, exponente = 2;
@@ -54,18 +56,37 @@ public class Triangulo {
 
         su_1 = mu_1 + mu_2;
         raiz = Math.sqrt(su_1);
-
+        
         int absoluto_1 = (int) Math.abs(raiz_cuadrada);
         int absoluto_2 = (int) Math.abs(raiz_cuad);
         int absoluto_3 = (int) Math.abs(raiz);
-
+        
+        double resta_0_1 = colum_2 - colum_1; 
+        double resta_1_0 = fil_2 - fil_1;
+        double div = resta_0_1 / resta_1_0;
+        
+        double resta_2_2 = colum_3 - colum_2; 
+        double resta_2_0 = fil_3 - fil_2;
+        double div_2 = resta_2_2 / resta_2_0;
+        
+        double resta_3_3 = colum_3 - colum_1; 
+        double resta_3 = fil_3 - fil_1;
+        double div_3 = resta_3_3 / resta_3;
+        
+        int resultado = (int) Math.abs(div);
+        int resultado_1 = (int) Math.abs(div_2);
+        int resultado_2 = (int) Math.abs(div_3);
+        
         System.out.println("Valor del punto 1 y 2: " + absoluto_1);
         System.out.println("Valor del punto 2 y 3: " + absoluto_2);
         System.out.println("Valor del punto 3 y 1: " + absoluto_3);
+        System.out.println(resultado);
+        System.out.println(resultado_1);
+        System.out.println(resultado_2);
         if (absoluto_1 == absoluto_2 && absoluto_2 == absoluto_3) {
             System.out.println();
             System.out.println("Equilatero");
-
+        
         } else if (absoluto_1 == absoluto_2 || absoluto_2 == absoluto_3 || absoluto_1 == absoluto_3) {
             System.out.println();
             System.out.println("Isosceles");
@@ -73,6 +94,55 @@ public class Triangulo {
         } else if (absoluto_1 != absoluto_2 && absoluto_2 != absoluto_3 && absoluto_1 != absoluto_3) {
             System.out.println();
             System.out.println("Escaleno");
+        
         }
+    }
+
+    public void setFil_1(int fil_1) {
+        this.fil_1 = fil_1;
+    }
+
+    public void setFil_2(int fil_2) {
+        this.fil_2 = fil_2;
+    }
+
+    public void setColum_1(int colum_1) {
+        this.colum_1 = colum_1;
+    }
+
+    public void setColum_2(int colum_2) {
+        this.colum_2 = colum_2;
+    }
+
+    public void setFil_3(int fil_3) {
+        this.fil_3 = fil_3;
+    }
+
+    public void setColum_3(int colum_3) {
+        this.colum_3 = colum_3;
+    }
+
+    public int getFil_1() {
+        return fil_1;
+    }
+
+    public int getFil_2() {
+        return fil_2;
+    }
+
+    public int getColum_1() {
+        return colum_1;
+    }
+
+    public int getColum_2() {
+        return colum_2;
+    }
+
+    public int getFil_3() {
+        return fil_3;
+    }
+
+    public int getColum_3() {
+        return colum_3;
     }
 }
