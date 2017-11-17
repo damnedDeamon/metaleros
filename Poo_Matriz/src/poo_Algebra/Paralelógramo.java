ff6be53fe400e760985f57e71b561a4387b165e7
josedonoso <j.donoso.catalan@gmail.com>
2017-11-16 22:37:05 -0300

    Avances clase Paralelógramo

---
+++ Poo_Matriz/src/poo_Algebra/Paralelógramo.java
@@ -0,0 +1,96 @@
+package poo_Algebra;
+
+public class Paralelógramo {
+
+    private int fila_1;
+    private int columna_1;
+    private int f_2;
+    private int c_2;
+    private int fil_3;
+    private int column_3;
+    private int f_4;
+    private int c_4;
+
+    public Paralelógramo(int fila_1, int columna_1, int f_2, int c_2, int fil_3, int column_3, int f_4, int c_4) {
+        this.fila_1 = fila_1;
+        this.columna_1 = columna_1;
+        this.f_2 = f_2;
+        this.c_2 = c_2;
+        this.fil_3 = fil_3;
+        this.column_3 = column_3;
+        this.f_4 = f_4;
+        this.c_4 = c_4;
+    }
+
+    public Paralelógramo() {
+    }
+
+    public int getFila_1() {
+        return fila_1;
+    }
+
+    public void setFila_1(int fila_1) {
+        this.fila_1 = fila_1;
+    }
+
+    public int getColumna_1() {
+        return columna_1;
+    }
+
+    public void setColumna_1(int columna_1) {
+        this.columna_1 = columna_1;
+    }
+
+    public int getF_2() {
+        return f_2;
+    }
+
+    public void setF_2(int f_2) {
+        this.f_2 = f_2;
+    }
+
+    public int getC_2() {
+        return c_2;
+    }
+
+    public void setC_2(int c_2) {
+        this.c_2 = c_2;
+    }
+
+    public int getFil_3() {
+        return fil_3;
+    }
+
+    public void setFil_3(int fil_3) {
+        this.fil_3 = fil_3;
+    }
+
+    public int getColumn_3() {
+        return column_3;
+    }
+
+    public void setColumn_3(int column_3) {
+        this.column_3 = column_3;
+    }
+
+    public int getF_4() {
+        return f_4;
+    }
+
+    public void setF_4(int f_4) {
+        this.f_4 = f_4;
+    }
+
+    public int getC_4() {
+        return c_4;
+    }
+
+    public void setC_4(int c_4) {
+        this.c_4 = c_4;
+    }
+
+    public void calculo() {
+        System.out.println("Test");
+    }
+
+}
